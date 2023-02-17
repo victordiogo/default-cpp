562b0e21956508e9c5eb072f3f62066d18baf509
Victor Diogo <victordiogo@victordioogo@gmail.com>
2023-02-17 20:39:47 -0300

    first commit

---
+++ tests/hello_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+TEST(hello_test, basic_assertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
